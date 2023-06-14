47e850f50c07062aba013b44ea49baa55eb60071
ye21st <ye21st@gmail.com>
2023-06-14 18:22:57 +0800

    feat: 提交代码

---
+++ fboot-oss/src/test/java/com/focre/fbootoss/FbootOssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.focre.fbootoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbootOssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
